ce307a89378e1d4dbe1144d59151c24c2579ea3d
niknuzhnyh <niknuzhnyh@gmail.com>
2021-02-07 23:24:26 +0200

    mob1

---
+++ var3/js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+   $(window).scroll(function () {
+      let scrolled = $(this).scrollTop(); /// значение текущего скролла
+
+      // console.log ( scrolled );
+
+      if (scrolled > 40) {
+         $(".header").css({
+            "background-color": "#0072FF",
+         });
+      } else if (scrolled < 40) {
+         $(".header").css({
+            "background-color": "transparent",
+         });
+      }
+      /// если скролл больше чем позиция элемента даём body доп класс и задаем элементу следующему за блоком #sticky отступ сверху равный высоте прилипшего блока
+   });
+});
+// !burger
+
+$(function () {
+   $('#burger').click(function(){
+      $('#mob-menu').toggleClass('active');
+      $('#burger--item').toggleClass('active');
+   })
+});
+
+// !burger-end
